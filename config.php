7638e65ac6523a9ff33d3b26ec4ec3ee5756fef1
Alevur <alevure@yandex.ru>
2022-01-10 14:17:13 +0400

    start

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Доступ запрещен');//запрет загрузки файла без предварительного обращения к index.php
+
+const SITE_URL = 'http://shop.loc';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = 'root';
+const DB_NAME ='shop';
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+define('VG_ACCESS', true); //запрет загрузки файла без предварительного обращения к index.php
+header('Content-Type:text/html; charset=utf-8'); //настройки для браузера
+session_start(); //старт сессии
+
+require_once 'config.php';
+require_once 'core/base/settings/internal_settings.php';
