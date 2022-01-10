9d4a2f9b6d3c15cb32302a7919a7d03965e8c264
Alevur <alevure@yandex.ru>
2022-01-10 16:14:31 +0400

    xdebug on

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 define('VG_ACCESS', true); //запрет загрузки файла без предварительного обращения к index.php
+
 header('Content-Type:text/html; charset=utf-8'); //настройки для браузера
 session_start(); //старт сессии
 
